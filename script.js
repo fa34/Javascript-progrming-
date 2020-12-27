dab4b7a03716711caca4b84663425fab8f6c97e3
farooq <farooqengineer29@gmail.com>
2020-12-27 20:30:40 +0500

    program 3
    The program03

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-let x=prompt("Input the Height in inches");
-c=2.54*x;
-alert(x);
-console.log("The Height in centimeters is" ,x);
+let x=prompt("Input the marks of English");
+let y=prompt("Input the marks of math");
+let z=prompt("Input the marks of URDU");
+let c=x+y+z;
+let avg=c/3;
+if(avg>80)
+{
+    alert(avg);
+}
